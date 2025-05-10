1655643bdcfac099a13e91811806a7f87fe25eda
FacundoOrona <fcndorona@gmail.com>
2025-05-10 17:21:50 -0300

    entidad Token

--- jwtapp/src/main/java/com/jwt/app/repository/Token.java
+++ jwtapp/src/main/java/com/jwt/app/repository/Token.java
@@ -1,5 +1,44 @@
 package com.jwt.app.repository;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tokens")
 public class Token {
+
+    public enum TokenType {
+        BEARER
+    }
+
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
     
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
 }
