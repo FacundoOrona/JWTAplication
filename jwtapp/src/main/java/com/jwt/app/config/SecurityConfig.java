f4b3129e993fd0d53385db8be344e38a3b329aba
FacundoOrona <fcndorona@gmail.com>
2025-05-11 22:42:01 -0300

    SecurityConfig impl

--- jwtapp/src/main/java/com/jwt/app/config/SecurityConfig.java
+++ jwtapp/src/main/java/com/jwt/app/config/SecurityConfig.java
@@ -2,18 +2,19 @@ package com.jwt.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-
+import com.jwt.app.repository.Token;
 import com.jwt.app.repository.TokenRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -41,8 +42,29 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilther, UsernamePasswordAuthenticationFilter.class)
-                ;
-
+                .logout(logout -> 
+                        logout.logoutUrl("/auth/logout")
+                                .addLogoutHandler((request, response, authentication) -> {
+                                    final var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+                                    logout(authHeader);
+                                })
+                                .logoutSuccessHandler((request, response, authentication) ->
+                                        SecurityContextHolder.clearContext())
+                );
         return hhtp.build();
     }
+
+    private void logout(final String token) {
+        if (token == null || !token.startsWith("Bearer ")){
+            throw new IllegalArgumentException("Invalid Token / token invalido");
+        }
+
+        final String jwtToken = token.substring(7);
+        final Token foundToken = tokenRepository.findByToken(jwtToken)
+                        .orElseThrow(() -> new IllegalArgumentException("Invalid token / token invalido"));
+        foundToken.setExpired(true);
+        foundToken.setRevoked(true);
+        tokenRepository.save(foundToken);
+    }
+
 }
--- jwtapp/src/main/java/com/jwt/app/repository/TokenRepository.java
+++ jwtapp/src/main/java/com/jwt/app/repository/TokenRepository.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
     List<Token> findAllByUserIdAndExpiredFalseAndRevokedFalse(Long userId);
+
+    Optional<Token> findByToken(String token);
 }
