5733cec81e1458a13041df4c19b1f71cb5b73c2f
FacundoOrona <fcndorona@gmail.com>
2025-05-11 22:02:08 -0300

    securityFilterChain impl

--- jwtapp/src/main/java/com/jwt/app/config/SecurityConfig.java
+++ jwtapp/src/main/java/com/jwt/app/config/SecurityConfig.java
@@ -1,5 +1,48 @@
 package com.jwt.app.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+
+import com.jwt.app.repository.TokenRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     
+    private final JwtAuthFilter jwtAuthFilther;
+    private final AuthenticationProvider authenticationProvider;
+    private final TokenRepository tokenRepository;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity hhtp) throws Exception {
+        hhtp
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/auth/**")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilther, UsernamePasswordAuthenticationFilter.class)
+                ;
+
+        return hhtp.build();
+    }
 }
